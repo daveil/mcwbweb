f37cb7701ac2999db78cc381c6949293d4b6598b
Dave Arroyo <arroyo.daveil@gmail.com>
2016-12-03 12:17:59 +0800

    Add page not found

--- pages/not-found.php
+++ pages/not-found.php
@@ -1,4 +1,10 @@
 <div class="container text-center">
-	<hr />
-	<p>Requested page <b><i><?php echo $page; ?></i></b> not found.</p>
+	<div class="no-content text-center">
+			
+			<p>
+				<strong>We're sorry.</strong>
+			Requested page "<b><i><?php echo $page; ?></i></b>" not found.<br/>
+			Return <a href="home">home</a>.
+			</p>
+	</div>
 </div>
