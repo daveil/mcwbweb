b0164c6b5b5b7335f3cba7fe2366d66cac475466
Dave Arroyo <arroyo.daveil@gmail.com>
2016-12-05 16:55:11 +0800

    Add operation banner

--- pages/health-services.php
+++ pages/health-services.php
@@ -18,17 +18,136 @@
 	<ul>
 		<li>Adult and Pediatric ICU</li>
 		<li>Ambulance</li>
-		<li>Complete Laboratory</li>
-		<li>Delivert Room</li>
 		<li>Emergency Room</li>
-		<li>Normal & Pathologic Nurses</li>
+		<li>Delivery Room</li>
 		<li>Operating Room</li>
+		<li>Laboratory</li>
+		<li>Normal & Pathologic Nurses</li>
 		<li>Pharmacy</li>
+		<li>Radiology Department</li>
 		<li>X-ray & CT Scan</li>
 	</ul>
 </section>
-<section id="specialists-banner" class="full-banner section">
+<section id="schedule-banner" class="full-banner section">
 	<img src="img/medium/doctor-consultation-banner.jpg" alt="" class="img-responsive"  width="100%"/>
+</section>
+<section id="schedules" class="container">
+	<h3>Schedules</h3>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Department</th>
+						<th>Weekdays</th>
+						<th>Saturday</th>
+						<th>Sunday</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>OPD Consultation</td>
+						<td>8 AM  - 5PM</td>
+						<td>8 AM  - 5PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>24 Hrs. Holter monitoring</td>
+						<td>8 AM  - 4PM</td>
+						<td>8 AM  - 4PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>2D Echo</td>
+						<td>8 AM  - 3PM</td>
+						<td>8 AM  - 3PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Stress Test</td>
+						<td colspan="3">By Appointment</td>
+					</tr>
+					<tr>
+						<td>EEG Unit</td>
+						<td>8 AM  - 4PM</td>
+						<td>8 AM  - 4PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Pulmonary Unit</td>
+						<td>8 AM  - 4PM</td>
+						<td>8 AM  - 4PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Ultrasound</td>
+						<td>
+							<small>MON</small>
+							<div>1PM - 3PM</div>
+							<small>TUE</small>
+							<div>9AM - 12PM</div>
+							<small>WED &mdash; FRI</small>
+							<div>11AM - 2PM</div>
+							
+						</td>
+						<td> 11AM &mdash; 2PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Mammography</td>
+						<td>8 AM  - 3PM</td>
+						<td>8 AM  - 3PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Endoscopy</td>
+						<td colspan="3">By Appointment</td>
+					</tr>
+					<tr>
+						<td>Laparoscopy</td>
+						<td colspan="3">By Appointment</td>
+					</tr>
+					<tr>
+						<td>Oncology Infusion Unit</td>
+						<td>8 AM  - 5PM</td>
+						<td>8 AM  - 5PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Dental Services</td>
+						<td colspan="3">By Schedule</td>
+					</tr>
+					<tr>
+						<td>Dialysis (NEPHRO)</td>
+						<td>8 AM  - Onwards</td>
+						<td>8 AM  - Onwards</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Physical Therapy</td>
+						<td >8 AM  - 4PM</td>
+						<td >8 AM  - 4PM</td>
+						<td>&mdash;</td>
+					</tr>
+					<tr>
+						<td>Occupational Therapy</td>
+						<td colspan="3">By Appointment</td>
+					</tr>
+					<tr>
+						<td>Speech Therapy</td>
+						<td colspan="3">By Appointment</td>
+					</tr>
+				</tbody>
+			</table>
+			<p>To reserve an appointment or check availability.</p>
+			<div id="book-wrapper" class="text-center">
+				<button class="btn btn-primary">Book Online</button>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="specialists-banner" class="full-banner section">
+	<img src="img/medium/operation-banner.jpg" alt="" class="img-responsive"  width="100%"/>
 </section>
 <section id="specialists" class="container section">
 	<h3>Specialists</h3>
@@ -93,56 +212,10 @@
 				<h4>R<span>ehabilitation</span> M<span>edicine</span></h4>
 				<ul>
 					<li class="separator"></li>
-					<li>Occupational Rehabi</li>
+					<li>Occupational medicine</li>
 				</ul>
 			</div>
 		</div>
 		
 	</div>
 </section>
-<section id="schedules" class="container">
-	<h3>Schedules</h3>
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Department</th>
-						<th>Weekdays</th>
-						<th>Saturday</th>
-						<th>Sunday</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>OPD Consultation</td>
-						<td>8 AM  - 5PM</td>
-						<td>8 AM  - 5PM</td>
-						<td>&mdash;</td>
-					</tr>
-					<tr>
-						<td>Holter monitoring</td>
-						<td>8 AM  - 5PM</td>
-						<td>&mdash;</td>
-						<td>&mdash;</td>
-					</tr>
-					<tr>
-						<td>2D Echo</td>
-						<td>8 AM  - 3PM</td>
-						<td>&mdash;</td>
-						<td>&mdash;</td>
-					</tr>
-					<tr>
-						<td>Stress Test</td>
-						<td colspan="3">By Appointment</td>
-					</tr>
-				</tbody>
-			</table>
-			<p>To reserve an appointment or check availability.</p>
-			<div id="book-wrapper" class="text-center">
-				<button class="btn btn-primary">Book Online</button>
-			</div>
-		</div>
-	</div>
-</section>
-
