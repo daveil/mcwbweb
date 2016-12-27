72252822f6b47f9fe9177794423b0cccc3f795da
Dave Arroyo <arroyo.daveil@gmail.com>
2016-12-27 11:48:09 +0800

    Add health-services and freedom-wall pages

--- wp-content/themes/mcwbsg/page.php
+++ wp-content/themes/mcwbsg/page.php
@@ -1,37 +1,13 @@
-<?php get_header();?>
-		<div class="container" id="content">
-			<?php 
-				if(have_posts()):
-					while(have_posts()):
-						the_post();
-			?>
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 content">
-					<div class="title">
-						
-						<h3 class="pull-left">
-							<?php the_title();?>
-						</h3>
-						<div class="pull-right share-button btn-group">
-							<button class="btn btn-default">
-								SHARE &nbsp;
-							<button class="btn btn-default">
-								COMMENT
-							</button>
-							<?php edit_post_link( 'Edit','', '',get_the_ID(),'btn btn-default' );?>
-						</div>
-						<div class="clearfix"></div>
-					</div>
-					<div style="padding-top:15px;">
-						<?php the_content();?>
-					</div>
-						
-				</div>
-			</div>
-			<?php 
-					endwhile;
-				endif;
-			?>
-		</div>
-<?php get_footer();?>
-
+<?php
+	get_header();
+	if ( have_posts() ):
+		while ( have_posts() ):
+			the_post();
+				the_content();
+	endwhile; else:
+?>
+    
+<?php
+	endif;
+	get_footer();
+?>
